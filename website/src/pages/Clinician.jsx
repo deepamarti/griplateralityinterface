ca83abc223ed6358ce2a46908d99c017c21b03de
BlackHawq <haq.hammud@gmail.com>
2023-04-27 22:29:30 -0700

    Adding DashBoard files.
    Adding Dashboard Files. Need to put them into a separate folder and remove some of the dummy data. Will Continue working on this tonight but it should be enough to start adding additional pages

---
+++ website/src/pages/Clinician.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Clinician = () => {
+  return (
+    <div>Clinician</div>
+  )
+}
+
+export default Clinician
---
+++ website/src/pages/TakeMeasure.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TakeMeasure = () => {
+  return (
+    <div>TakeMeasure</div>
+  )
+}
+
+export default TakeMeasure
