e4d81f211064fc006e7cd897e67b3a0ff3621124
Sophie Mi <smsophiemi@gmail.com>
2023-05-06 15:42:06 -0700

    display and update graph

--- html_website/data.js
+++ html_website/data.js
@@ -8,3 +8,16 @@ function Initialize() {
     document.getElementById('ble_collection').style.visibility = 'hidden';
     document.getElementById('manual_collection').style.visibility = 'hidden';
 }
+
+let isBLE = false;
+function SetCollectionMethod(flag=false) {
+    isBLE = flag;
+    if (isBLE) {
+        document.getElementById('ble_collection').style.visibility = 'visible';
+        document.getElementById('manual_collection').remove();
+    } else {
+        document.getElementById('manual_collection').style.visibility = 'visible';
+        document.getElementById('ble_collection').remove();
+    }
+
+}
