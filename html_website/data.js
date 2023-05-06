0651a7fe54ceafc043a1059634593fe62d61baeb
Sophie Mi <smsophiemi@gmail.com>
2023-05-06 14:12:51 -0700

    patient info display, method selection logic added

--- html_website/data.js
+++ html_website/data.js
@@ -2,3 +2,9 @@ function SayHi() {
     alert("hi");
     document.getElementById('say_hi').innerText = "hello";
 }
+
+function Initialize() {
+    document.getElementById('select_collection_method').style.visibility = 'visible';
+    document.getElementById('ble_collection').style.visibility = 'hidden';
+    document.getElementById('manual_collection').style.visibility = 'hidden';
+}
