c3402626f5cf4a4593f6ee43daab665b22e00936
Sophie Mi <smsophiemi@gmail.com>
2023-05-16 04:03:33 -0700

    basic logic for enable/disable ble/man

--- docs/data.js
+++ docs/data.js
@@ -12,7 +12,7 @@ function Initialize() {
     SwitchTab(event,'Bluetooth');
 }
 
-let isBLE = false;
+
 function SetCollectionMethod(flag=false) {
     isBLE = flag;
     if (isBLE) {
@@ -24,25 +24,75 @@ function SetCollectionMethod(flag=false) {
     }
 
 }
-
+let is_BLE_selected = false;
+let is_method_locked = false;
 // switch between ble and man tabs
 function SwitchTab(evt, TabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
+    
+    if (!is_method_locked){
+       if (TabName == 'Bluetooth') {
+            is_BLE_selected = true;
+            is_method_locked = true;
+        } else if (TabName == 'Manual') {
+            is_BLE_selected = false
+            is_method_locked = true;
+        } 
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(TabName).style.display = "block";
+        evt.currentTarget.className += " active";
+    } else {
+        alert(TabName);
+        alert(is_BLE_selected);
+        if ((TabName =='Manual' && is_BLE_selected) || (TabName =='Bluetooth' && !is_BLE_selected)) {
+            // confirm if user wants to switch collection method
+            let msg_text = "";
+            if (TabName == 'Manual') {
+                msg_text = "You are about to switch from bluetooth to manual entry. Do you want to proceed?"
+            } else {
+                msg_text = "You are about to switch from manual to bluetooth entry. Do you want to proceed?"
+            }
+            if (confirm(msg_text)) {
+                // Get all elements with class="tabcontent" and hide them
+                tabcontent = document.getElementsByClassName("tabcontent");
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
+
+                // Get all elements with class="tablinks" and remove the class "active"
+                tablinks = document.getElementsByClassName("tablinks");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+
+                // Show the current tab, and add an "active" class to the button that opened the tab
+                document.getElementById(TabName).style.display = "block";
+                evt.currentTarget.className += " active";
+
+                if (TabName == 'Bluetooth') {
+                    is_BLE_selected = true;
+                    is_method_locked = false;
+                } else if (TabName == 'Manual') {
+                    is_BLE_selected = false
+                    is_method_locked = false;
+                } 
+            }
+        }
     }
+    
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(TabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    
     }
