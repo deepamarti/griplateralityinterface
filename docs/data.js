af53afb269ac01e9bfe7f9ae19b3b6b0323ef85c
Sophie Mi <smsophiemi@gmail.com>
2023-05-16 03:43:14 -0700

    data collection tab widths

--- docs/data.js
+++ docs/data.js
@@ -8,6 +8,8 @@ function Initialize() {
     //document.getElementById('select_collection_method').style.visibility = 'visible';
     //document.getElementById('ble_collection').style.visibility = 'hidden';
     //document.getElementById('manual_collection').style.visibility = 'hidden';
+
+    SwitchTab(event,'Bluetooth');
 }
 
 let isBLE = false;
@@ -22,3 +24,25 @@ function SetCollectionMethod(flag=false) {
     }
 
 }
+
+// switch between ble and man tabs
+function SwitchTab(evt, TabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(TabName).style.display = "block";
+    evt.currentTarget.className += " active";
+    }
