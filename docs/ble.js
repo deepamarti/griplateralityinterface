d55151626d6b1ef894926dff36def1c9c3896b1b
Sophie Mi <smsophiemi@gmail.com>
2023-05-23 05:15:59 -0700

    moved chart.js into ble.js

--- docs/ble.js
+++ docs/ble.js
@@ -1,5 +1,8 @@
 // ble data collection
 
+import Chart from 'chart.js/auto';
+
+// declare all html objects
 const btn_connect = document.getElementById('ble_connect');
 const btn_disconnect = document.getElementById("ble_disconnect");
 const btn_collect_0 = document.getElementById('ble_right_t1');
@@ -10,6 +13,9 @@ const btn_collect_4 = document.getElementById('ble_left_t2');
 const btn_collect_5 = document.getElementById('ble_left_t3');
 const ble_status = document.getElementById('ble_status');
 
+const graph = document.getElementById("ble_graph_sample");
+
+// declare all global variables
 let gdxDevice = "";
 let current_index = null;
 let sample_data = Array(6);
@@ -20,6 +26,12 @@ let left_num_samples = 0;
 
 let done_sampling = false;
 
+let graph_interval = null;
+const time = Array.from(
+    { length: 51 },
+    (value, index) => Math.round(index * 0.1 *10)/10);
+
+
 const ConnectDevice = async() => {
     try {
         const bleDevice = await navigator.bluetooth.requestDevice({
@@ -29,7 +41,7 @@ const ConnectDevice = async() => {
         gdxDevice = await godirect.createDevice(bleDevice,  {open: true, startMeasurements: false});
         ble_status.style.backgroundColor = "#abd699";
         console.log(`Connected to: ${gdxDevice.name}`);
-       //graph_interval = setInterval(updateValue, 100);
+        graph_interval = setInterval(updateValue, 100);
         disable_button('ble_connect');
         enable_button('ble_disconnect');
         lock_method_selection();
@@ -47,7 +59,7 @@ function DisconnectDevice() {
         }
         gdxDevice = "";
         ble_status.style.backgroundColor = "#ffe26a";
-        //clearInterval(graph_interval);
+        clearInterval(graph_interval);
         enable_button('ble_connect');
         disable_button('ble_disconnect');
     } catch (err) {
@@ -102,6 +114,58 @@ btn_collect_4.index = 4;
 btn_collect_5.addEventListener('click', CollectData);
 btn_collect_5.index = 5;
 
+let chart = new Chart.Line(graph, {
+    data: {
+        labels: time,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "#75c9b7",
+            borderColor: "#75c9b7",
+            borderWidth: 1,
+            order: 1,
+            data: sample_data[0]
+        }, {
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "#ea8a81",
+            borderColor: "#ea8a81",
+            borderWidth: 1,
+            order: 0,
+            data: opt_sample_data[0]
+        }]
+    },
+    options: {
+        legend: {display: false},
+        scales: {
+        yAxes: [{ticks: {min: -1, max:600}}],
+        },
+        tooltips: {enabled: false}
+    }
+});
+
+function updateValue() {
+    //force_data = sample_data[current_index];
+    if (done_sampling == true) {
+        opt_sample_time[current_index]= FindOptSample(force_data);
+        for (let i=0;i<5;i++){
+            opt_data[opt_sample_time[current_index][i]*10] = force_data[opt_sample_time[current_index][i]*10];
+            opt_sample_data[current_index][i] = opt_data[opt_sample_time[current_index][i]*10];
+            AddToOptTable(opt_sample_time[current_index][i], opt_sample_data[current_index][i]);
+        }
+        // disable everything except for the accept reject buttons
+        //WaitForAcceptReject();
+        //
+        done_sampling=false;
+
+        // do accept reject here, and increment counters
+        // disable buttons in accept reject listeners
+        
+    }
+    chart.data.datasets[0].data = sample_data[current_index];
+    chart.data.datasets[1].data = opt_sample_data[current_index];
+    chart.update();
+}
 /*
 
 <!--
